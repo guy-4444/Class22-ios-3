3443e05b4bcc0293d25c5a00f5fecce5fd56c149
guymacin <guyskv@gmail.com>
2022-04-04 17:15:30 +0300

    Initial Commit

---
+++ Class22-ios-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Class22-ios-3
+//
+//  Created by Guy Macin on 04/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
